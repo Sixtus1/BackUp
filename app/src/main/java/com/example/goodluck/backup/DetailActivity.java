9d7259af1be637d7c24646cd0b10a092c57e02e6
Sixtus <gluck1992@gmail.com>
2015-10-15 17:31:01 +0300

    initialize repo

---
+++ app/src/main/java/com/example/goodluck/backup/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.goodluck.backup;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class DetailActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/example/goodluck/backup/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.goodluck.backup;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
+    ListView backup_list=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      backup_list=(ListView) findViewById(R.id.backup_list);
+        //creating list Adapter
+        List<String> listAdapter=new ArrayList<String>();
+        //list components
+        String components[]=new String[3];
+        //mentioning those components
+        components[0]="---TESTS---";
+        components[1]="---RESULTS---";
+        components[2]="---TEACHERS---";
+        //creating for loop
+        for(String component:components){
+          listAdapter.add(component);
+        }
+       ListAdapter adapter= new ArrayAdapter<>(this,
+               android.R.layout.simple_list_item_1,listAdapter);
+        backup_list.setAdapter(adapter);
+
+        //creating on click listener.
+        backup_list.setOnItemClickListener(this);
+
+    }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        int x=position+1;
+        //Toast.makeText(getApplicationContext(),"under trial "+x,Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+
+    }
+}
+
+
